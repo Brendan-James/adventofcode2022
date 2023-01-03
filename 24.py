e6dc3f93f9b05c9ea99977f9f72aa37acac7b32d
Brendan-James <35391207+Brendan-James@users.noreply.github.com>
2023-01-02 22:13:34 -0800

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,114 @@
+import copy
+data = """#.####################################################################################################
+#<v>v><>>>v>v^<<<^><^>>^..>^^^><vv^>^><<.<^<^v<v^><<.v^>^vv.^v^>^>.^>^<<v^>v>v>>v><^..^.^<v^.>v..^v^>#
+#><v^v.v.<>>><v<>.^^>>^^>^>><<v.v.^<<<<<><<<^v><v<<v^><.<v<v^^v^v^^^<^v><>>.v.vvv<>v<<v<<<<vv>>><^^^>#
+#><v^>.v.<>>vvv^.>^<v>v<^^^.^.><^vv<v>>^..>^>>>><v.<^v^><^^v>^v<v><vv>^>v<<>>.^>^^^>>vv>v>..^>..v<<<<#
+#<^>v><v<^^vv<<>>^<<^^<^^^^<.<..<v<>..>v..^v<<vvv>.v<v<.^^>^<<>>^^>.<^^>><>^<.><v<>vv.^<^^^.v<^>v^^^>#
+#>vv^<^.^^^.^.><<<^vv>^<^<>v>>^^^.v<><v^<v<v<^^>><>vv.v.<<>>><<^<v^>><<.><><<>^v^<>v>^vv>^><>.v><v>.<#
+#<^<v>^v<>^<<<vvv<^^^><.v<<vv>v.>^v^^^^>v><^>v<^^.v^^v<>v.>><.<<^^>>.^<>>v^^v<.><<>v>^^^v>v^<v>><v^<.#
+#<v<>^<>.<.<vvv.>^^v^v^.v>>>v>.^.vv<^^^>^<>>.<^<<v<v^>><<>^^vv><<<<vv.^^>><^v><vv^v^>>^>v>v>^<>v^v<><#
+#<>><><^><v^.<..^vv^^v><.v<vv<<>^v^>><>v>^>.<.>>.v<^v^v.^<<<.v^.<>>.^v<..^.v^.v^v^^^>>^vvv.<.^<<>.<<>#
+#><>>vv^v^.>v^^v<vv><<.>v<^><^v>><vvv>.^v>><>^.>><^<>^>^>v>^>^^.>><^>^..<^<><^^<^><><<v><.>.<.<<v^.^>#
+#>^>v^v>v<^v>>^vv^v<vv<^^<>.>^.vv^>>^<^>^^vvv^>v^^^><.<vv>>><>^.v<^<<<.v<<<<<>.v>>^>^vv>^^^^>^><^v^^.#
+#>^v<>v<v>><.vv^^>>.>v^>>^.>>^><<^<<v^>>>v^><>.^v.v^^>v><^v<><^.^vv.vv.v>.v<v<><^<v<v^>^>>vv<>^^v^>^>#
+#<<>vv<.>v<v>.<<^<>v>.^..<^><<>>v>^>v<>v^<>.vv>v>>^.>>>>v<^^.>^^<<v^<^^>.<^v^>.v^v<^<.<<v.v<<<^v^<<><#
+#><v^^v>>vv>^.<vv<^v^..^<<.>^<^.v><>>vv<.^<>.<.v.v<vv^>>v^<>^<^v.^<><<>^^^.><>vv>><.^v^^<^v<^vv>>v^<>#
+#>^>>v<^<<^>><<^.^>>>v^>vv<<<><^^>><v>>>>.v<v>.<v^v.^v>^^>^>^^>.^<>><<vvvv>^<^<.><v<.^^>.^^^<^vvvv<>>#
+#>.>>>v^>>>^.^<^>^<v>.v.^.^><^><^^v<v>.v<<<>>>v>v^v^>v^<>^>v<<^>v^^><<<>v^v.^^.^.v>vv>.^>v><v><v<>v<>#
+#>v<>^<^.<<v><>vv^>>^^<<^^>v><><v<><<^><<>v<<<^^^>v>>v.v<v>><<^^^<^<vvv>v^^>>^><<>>v<v^v>>><.>.>v>>><#
+#<v^<<vv^v^>^<.^>v<^v><^^<v>>v.<^<^v^^.>^v>v^><vvv^.<^<^>v^^>><<v>>><v<^^^^v^v>^>.<>^v>>.<<<^<^v^<^v<#
+#>.>v<v.>>><.<^<.^>v><v>.v>^^<<^>v<^<.<>vv<v<vvv<vv><v^>^v>>.v<^><<.<<><>>>>^<<<^.>><><v.<<>vv<><^><>#
+#>v>vv<<^<<v>^<v>><^<>>><^><v^v^>v>.^<>vv<>v<^.<^<><<.<>v><>><<v.^<><v^^>^><<^v<.^<^>^<v^.v^.<>^v^v>>#
+#>v^v.<v^<>v.><.^>v>vv<v^^.><><v>...>^.>^.>>>^^><^>.<><<^.^.vvv^v^<vvv^.>v>.^<<<v<^<.v.>vv<v.^v.>><.<#
+#>.^.^><^<.<>v><.><<vv^^^>><.>><v.^.^<.<^<^>>>v^vv<v^<^vv<vv^^>^>v><^<.>^<vv<.<<v>>v><v^>^>v^^><^^<^>#
+#<v<^.<v<^.<<>^^v.^^..vv>>vv>><^^>v^>v<.<><>.^>v^vv.<v.v^^^vv<^><v^.<<>^<v>.^<<v>..v><.^v^^<>>^^>.<<>#
+#>v^<<vv<v<<>v<<v>^v>v><^>v><v^v^^<v^vv<v^>v<>vv>^v<<vv.<vv<v^.<<<>>^v<v<v^<.v^.<^<><<<^vvv<<^<>^v^>>#
+#>>>v<^>>v>.<><<<>.<v>>.^vv<<vvv>>vv^.^vv>^vv<v>.v<.vv>v>>>>..vv<vv.v^vv^.vv>^<^>^^...^vv^vv>>^v<v>^<#
+#>v>.vvv<>v^.^<<v<v^<>v^.>v<^vv.<^<<v^v^^v>v^^^>^><<.^^^^v>vv<<<>.v<v>^>>v>v<>v><^vv.<<^>^v><v>^^>v<>#
+#<.<>^.<vv^^><<v<.<<^<>v<>v><v>>>.><<<>.^<v<><^vv<.^<.vv<>vv<v^v.^v^vv^^><^<v<>>^<.>vvv<^<<vv><<><^.<#
+#>^vv<.><<^<v.<^>><^>>^^>v>>v.v.^>v>^<^>><^v>.vvvv<>v>^^>vv.<>v>^<<<^.v^v^^^^>^.vv..^^>.>>^><^>v^v>^>#
+#>^<<v<>.><^<>v^<<>^^>v>^>vv^><.v^<>>^v.>^v^<>.v^v<.^>^^v.v<>v<^>>^^>^vv.<v>>^v<<.^><^<^^vv>>vvv^^v..#
+#.<^<v><<>^v.<<.<^vvv>vv>.v>vv<^<<<<<v<<>^vv>vv<v.^<<.^^vv<<><^<.^>^.^>v<<>^>v^^^>^>.<>^v<^<>v<v<^^^<#
+#<>v<><><^.^v.>>>.<^>>^>^>v^<vv^vvv^^<^>^^.^<.<>vv>^<><vv^vv>><><vvv<v>v>^<v>>.v<>v^>^>v.<v.<.>v.>^v<#
+#>v^>^>v^.<v^<>vv<.^>v.v<v<>v>.^>^>^^><<^^v<>v<^vv><>^^^<<<vv><><<>^<.>.vv^>vv<<<<<v><v.^vv>vvv<v^v>>#
+#>vv<<vv.^<<><<<v^^^>^v<>>v^..v.<<^v<<<<^.<<<<^>>v.<^^<<^v>v^><.>v<^>><v<>^.v^vv<<<v<<^>>^^vv>^>v>>v>#
+#>>.^v>>v^v^>^^.v^v<v<v^.<>>^v>>><v<<<>>>vv><vv>>^>^v><>^^>><v.^<^^^.v^v<>>vv>v>^>vv.^v>vv<v>>.>^v<v>#
+#<^<^.>.><v^.v>>v.<<>v>^.v^<<v><><><>^v^v<>^<v><>>.vv<<^v<<>^^v^^^>>.<<^^v<<vv^<v>^>v<v^^v>>.v>vv>><<#
+#><v^<^.v<<^^<><>>v.<<<v^<v.^<^>>.>.>^^^^<v<^<>^vvv><><<>>>v^v.^.><>><.^<>..><>^.><<v><.^^>v>^.>>>v>>#
+####################################################################################################.#""".split("\n")
+
+blizzards = []
+width = len(data[0])
+height = len(data)
+for x in range(1,width-1):
+    for y in range(1,height-1):
+        if data[y][x]!=".":
+            blizzards.append([x,y,data[y][x]])
+
+def step(blizzards,width,height):
+    gizzards = copy.deepcopy(blizzards)
+    for i in gizzards:
+        if i[2]=="^":
+            i[1]-=1
+            if i[1]==0:
+                i[1] = height-2
+        if i[2]=="v":
+            i[1]+=1
+            if i[1]==height-1:
+                i[1] = 1
+        if i[2]=="<":
+            i[0]-=1
+            if i[0]==0:
+                i[0] = width-2
+        if i[2]==">":
+            i[0]+=1
+            if i[0]==width-1:
+                i[0] = 1
+    return gizzards
+
+positions = [[1,0]]
+steps = 0
+project = 0
+while True:
+    #print(steps,len(positions))
+    steps+=1
+    blizzards = step(blizzards,width,height)
+    newpositions = set()
+    for i in positions:
+        newpositions.add(str(i[0])+","+str(i[1]))
+        newpositions.add(str(i[0]+1)+","+str(i[1]))
+        newpositions.add(str(i[0]-1)+","+str(i[1]))
+        newpositions.add(str(i[0])+","+str(i[1]+1))
+        newpositions.add(str(i[0])+","+str(i[1]-1))
+    spaces = set()
+    for i in blizzards:
+        spaces.add(str(i[0])+","+str(i[1]))
+    positions = []
+    for i in newpositions:
+        if i not in spaces:
+            temp = i.split(",")
+            temp[0] = int(temp[0])
+            temp[1] = int(temp[1])
+            if 0<temp[0]<width-1 and 0<temp[1]<height-1:
+                positions.append([temp[0],temp[1]])
+                continue
+            if temp[0]==1 and temp[1]==0:
+                if project == 1:
+                    print(steps)
+                    positions = [[1,0]]
+                    project = 2
+                    break
+                else:
+                    positions.append([temp[0],temp[1]])
+                continue
+            if temp[0]==width-2 and temp[1]==height-1:
+                if project == 0:
+                    print("a:",steps)
+                    positions = [[width-2,height-1]]
+                    project = 1
+                    break
+                elif project == 2:
+                    print("b:",steps)
+                    assert False
+                else:
+                    positions.append([temp[0],temp[1]])
